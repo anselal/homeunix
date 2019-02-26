4019c80132a8a2c46f37105d04a2a4ed0a06aa9e
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2019-02-26 22:24:24 +0200

    :up: update(dwm): Update binding for dmenu

--- dwm/config.h
+++ dwm/config.h
@@ -79,7 +79,7 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier               key              function        argument */
-	{ MODKEY,                 XK_KP_Insert,    spawn,          {.v = dmenucmd } },
+	{ MODKEY,                 XK_z,            spawn,          {.v = dmenucmd } },
 	{ MODKEY,                 XK_Return,       spawn,          {.v = termcmd } },
 	{ MODKEY,                 XK_b,            togglebar,      {0} },
 	{ MODKEY|ShiftMask,       XK_j,            rotatestack,    {.i = +1 } },

06395bbf1812de5e39e21cd2b205e35cd1e2cb4e
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2019-03-01 15:09:50 +0200

    :up: update(dwm): Add FreeBSD brand to dmenu

--- dwm/config.h
+++ dwm/config.h
@@ -75,7 +75,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-p", N_FREEBSD, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {

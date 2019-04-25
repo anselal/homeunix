797c7e5c56048ae9f37fad116e95102af221c92a
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2019-04-25 14:31:39 +0300

    :up: update(post-install)

--- post-install.sh
+++ post-install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-echo -n "[INFO] Enabling i915kms..."
+awk -F, '{ print "[INFO] Installing "$2 system("sudo pkg install -y "$1)}' progs.csv
+
+echo "[INFO] Enabling i915kms..."
 sleep 1
 cat <<EOF >> /etc/rc.conf
 
 kld_list="/boot/modules/i915kms.ko"
 EOF
 
+echo "[INFO] Setting UTF-8 locale"
+sleep 1
+cat <<EOF >> /etc/profile
 
+LANG=en_US.UTF-8;   export LANG
+CHARSET=UTF-8;  export CHARSET
+EOF
